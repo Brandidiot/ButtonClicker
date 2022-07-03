98fa7b365a5cc3dd651ad859d18214da373cb404
Brandidiot <brandon_olah@yahoo.com>
2022-07-03 18:26:13 -0400

    Fixed Upgrade Scriptable Object. Added More Upgrades.
    Fixed Required Building Level For Upgrade To Be Correct. Added More Farm, Mine, & Factory Upgrades. Cleaned Code.

--- ButtonClicker/Assets/Scripts/Upgrade/UpgradesManager.cs
+++ ButtonClicker/Assets/Scripts/Upgrade/UpgradesManager.cs
@@ -1,8 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+
+
+/*
+ * Instead of storing all upgrades under the upgrade game object. It might be more beneficial to store all
+ * upgrades as prefabs and edit this manager to instantiate a new child object if the prefab is purchasable.
+ */
+
 
 public class UpgradesManager : MonoBehaviour
 {
@@ -10,8 +13,10 @@ public class UpgradesManager : MonoBehaviour
 
     private void Start()
     {
+        //Get # of upgrades and set upgrades array to amount of upgrades
         upgrades = new Upgrade[transform.childCount];
         
+        //set upgrades
         for (var i = 0; i < transform.childCount; i++)
         {
             var child = transform.GetChild(i).GetComponent<Upgrade>();
@@ -27,12 +32,18 @@ public class UpgradesManager : MonoBehaviour
 
     private void CheckUpgrades()
     {
+        //Loop through all upgrades
         foreach (var upgrade in upgrades)
         {
-
+            
+            //If not enough money for the upgrade go to next upgrade
             if (!(GameManager.Instance.currentScore >= upgrade.upgradeItem.BaseCost)) continue;
-            if (upgrade.building.buildingLevel < upgrade.upgradeItem.requiredBuildingLevel) continue;
-            if (!upgrade._purchased)
+            
+            //if building level is lower than the required level than go to next upgrade
+            if (upgrade.building.buildingLevel < upgrade.upgradeItem.requiredBuildingLevel + 1) continue;
+            
+            //If the upgrade isn't purchased than show the upgrade to purchase
+            if (!upgrade.purchased)
             {
                 upgrade.gameObject.SetActive(true);
             }
